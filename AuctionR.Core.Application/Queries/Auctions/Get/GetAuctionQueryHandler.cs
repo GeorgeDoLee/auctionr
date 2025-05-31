81b9d4097ffe5fab5697300380692fec4675e7db
Giorgi Dolidze <gio.dolidze1234@gmail.com>
2025-05-31 15:25:36 +0400

    added GetAuctionQuery, its validator and handler

---
+++ AuctionR.Core.Application/Queries/Auctions/Get/GetAuctionQueryHandler.cs
@@ -0,0 +1,28 @@
+﻿using AuctionR.Core.Application.Models;
+using AuctionR.Core.Domain.Interfaces;
+using Mapster;
+using MediatR;
+
+namespace AuctionR.Core.Application.Queries.Auctions.Get;
+
+public class GetAuctionQueryHandler : IRequestHandler<GetAuctionQuery, AuctionModel?>
+{
+    private readonly IUnitOfWork _unitOfWork;
+
+    public GetAuctionQueryHandler(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task<AuctionModel?> Handle(GetAuctionQuery query, CancellationToken ct)
+    {
+        var auction = await _unitOfWork.Auctions.GetAsync(query.Id, ct);
+
+        if (auction == null)
+        {
+            return null;
+        }
+
+        return auction.Adapt<AuctionModel>();
+    }
+}
---
+++ AuctionR.Core.Application/Queries/Auctions/Get/GetAuctionQueryValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace AuctionR.Core.Application.Queries.Auctions.Get;
+
+public class GetAuctionQueryValidator : AbstractValidator<GetAuctionQuery>
+{
+    public GetAuctionQueryValidator()
+    {
+        RuleFor(x => x.Id)
+            .NotEmpty().WithMessage("Id is required.")
+            .GreaterThan(0).WithMessage("Id must be greater than 0.");
+    }
+}
