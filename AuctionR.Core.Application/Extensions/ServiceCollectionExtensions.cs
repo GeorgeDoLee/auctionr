520d31e18a45ebf60d9268bbc8bf48ac1d2dcbaa
Giorgi Dolidze <gio.dolidze1234@gmail.com>
2025-05-31 14:29:38 +0400

    added AddApplication extension method and registered mapster, fluentvalidator and mediatr

---
+++ AuctionR.Core.Application/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+using FluentValidation.AspNetCore;
+using Mapster;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AuctionR.Core.Application.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        var appAssambly = typeof(ServiceCollectionExtensions).Assembly;
+
+        services.AddMediatR(config =>
+        {
+            config.RegisterServicesFromAssembly(appAssambly);
+        });
+
+        services.AddValidatorsFromAssembly(appAssambly)
+            .AddFluentValidationAutoValidation();
+
+        TypeAdapterConfig.GlobalSettings
+            .Scan(typeof(ServiceCollectionExtensions).Assembly);
+
+        return services;
+    }
+}
---
+++ AuctionR.Core.Application/Mapping/MappingConfig.cs
@@ -0,0 +1,23 @@
+﻿using AuctionR.Core.Application.Commands.Auctions.Create;
+using AuctionR.Core.Domain.Entities;
+using AuctionR.Core.Domain.Enums;
+using Mapster;
+
+namespace AuctionR.Core.Application.Mapping;
+
+public class MappingConfig : IRegister
+{
+    public void Register(TypeAdapterConfig config)
+    {
+        config.NewConfig<CreateAuctionCommand, Auction>()
+            .Map(dest => dest.ProductId, src => src.ProductId)
+            .Map(dest => dest.StartingPrice, src => src.StartingPrice)
+            .Map(dest => dest.StartTime, src => src.StartTime)
+            .Map(dest => dest.EndTime, src => src.EndTime)
+            .Map(dest => dest.HighestBidAmount, _ => 0m)
+            .Map(dest => dest.Status, _ => AuctionStatus.Pending)
+            .Ignore(dest => dest.Id)
+            .Ignore(dest => dest.Bids!)
+            .Ignore(dest => dest.HighestBidderId!);
+    }
+}
