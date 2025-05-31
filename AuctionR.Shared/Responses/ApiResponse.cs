d4a091ba0ee9d08802cf43dc4dbf627e382cfe7d
Giorgi Dolidze <gio.dolidze1234@gmail.com>
2025-05-31 16:26:59 +0400

    edited ApiResponse

--- AuctionR.Shared/Responses/ApiResponse.cs
+++ AuctionR.Shared/Responses/ApiResponse.cs
@@ -2,7 +2,7 @@
 
 public record ApiResponse<T>(bool Success, string? Message = null, T? Data = default)
 {
-    public static ApiResponse<T> SuccessResponse(T data, string? message = null) =>
+    public static ApiResponse<T> SuccessResponse(string message, T? data = default) =>
         new(true, message, data);
 
     public static ApiResponse<T> FailResponse(string message, T? data = default) =>
