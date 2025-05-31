33e013496c61aedbad315ce0e8447e95134f0d36
Giorgi Dolidze <gio.dolidze1234@gmail.com>
2025-05-31 15:49:39 +0400

    added GetAllAuctionsQuery and its handler

---
+++ AuctionR.Core.Application/Queries/Auctions/GetAll/GetAllAuctionsQuery.cs
@@ -0,0 +1,8 @@
+﻿using AuctionR.Core.Application.Models;
+using MediatR;
+
+namespace AuctionR.Core.Application.Queries.Auctions.GetAll;
+
+public class GetAllAuctionsQuery : IRequest<IEnumerable<AuctionModel>>
+{
+}
---
+++ AuctionR.Core.Application/Queries/Auctions/GetAll/GetAllAuctionsQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿using AuctionR.Core.Application.Models;
+using AuctionR.Core.Domain.Interfaces;
+using Mapster;
+using MediatR;
+
+namespace AuctionR.Core.Application.Queries.Auctions.GetAll;
+
+public class GetAllAuctionsQueryHandler :
+    IRequestHandler<GetAllAuctionsQuery, IEnumerable<AuctionModel>>
+{
+    private readonly IUnitOfWork _unitOfWork;
+
+    public GetAllAuctionsQueryHandler(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task<IEnumerable<AuctionModel>> Handle(
+        GetAllAuctionsQuery request, CancellationToken ct)
+    {
+        var auctions = await _unitOfWork.Auctions.GetAllAsync();
+
+        return auctions.Adapt<List<AuctionModel>>();
+    }
+}
