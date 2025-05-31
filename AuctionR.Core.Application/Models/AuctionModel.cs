425b1f16dc8792e8d425eaf317ed3638b3cb5663
Giorgi Dolidze <gio.dolidze1234@gmail.com>
2025-05-31 14:30:18 +0400

    added auction and bid models

---
+++ AuctionR.Core.Application/Models/AuctionModel.cs
@@ -0,0 +1,24 @@
+﻿using AuctionR.Core.Domain.Enums;
+
+namespace AuctionR.Core.Application.Models;
+
+public class AuctionModel
+{
+    public int Id { get; set; }
+
+    public int ProductId { get; set; }
+
+    public decimal StartingPrice { get; set; }
+
+    public DateTime StartTime { get; set; }
+
+    public DateTime EndTime { get; set; }
+
+    public decimal HighestBidAmount { get; set; }
+
+    public int? HighestBidderId { get; set; }
+
+    public AuctionStatus Status { get; set; }
+
+    public IEnumerable<BidModel>? Bids { get; set; }
+}
---
+++ AuctionR.Core.Application/Models/BidModel.cs
@@ -0,0 +1,14 @@
+﻿namespace AuctionR.Core.Application.Models;
+
+public class BidModel
+{
+    public int Id { get; set; }
+
+    public int AuctionId { get; set; }
+
+    public int BidderId { get; set; }
+
+    public decimal Amount { get; set; }
+
+    public DateTime Timestamp { get; set; }
+}
