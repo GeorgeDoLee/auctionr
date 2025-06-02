f296a2b2a20e29ef5c9f0ee14be4e5085ad1b8be
Giorgi Dolidze <gio.dolidze1234@gmail.com>
2025-06-02 16:33:19 +0400

    override GetAsync method in auction repository

--- AuctionR.Core.Infrastructure/Repositories/AuctionRepository.cs
+++ AuctionR.Core.Infrastructure/Repositories/AuctionRepository.cs
@@ -2,13 +2,22 @@
 using AuctionR.Core.Domain.Interfaces;
 using AuctionR.Core.Infrastructure.Persistance;
 using AuctionR.Shared.Repositories;
+using Microsoft.EntityFrameworkCore;
 
 namespace AuctionR.Core.Infrastructure.Repositories;
 
 internal class AuctionRepository : Repository<AuctionRDbContext, Auction>, IAuctionRepository
 {
+
     public AuctionRepository(AuctionRDbContext context) 
         : base(context)
     {
+    }
+
+    public override async Task<Auction?> GetAsync(int id, CancellationToken ct = default)
+    {
+        return await _context.Auctions
+            .Include(a => a.Bids)
+            .FirstOrDefaultAsync(a => a.Id == id, ct);
     }
 }
