702325c0c7e6f72c28d950aaa20ecf70545383d9
Giorgi Dolidze <gio.dolidze1234@gmail.com>
2025-06-03 14:03:52 +0400

    added GetByAuctionIdAsync repository method

--- AuctionR.Core.Domain/Interfaces/IBidRepository.cs
+++ AuctionR.Core.Domain/Interfaces/IBidRepository.cs
@@ -5,4 +5,7 @@ namespace AuctionR.Core.Domain.Interfaces;
 
 public interface IBidRepository : IRepository<Bid>
 {
+    Task<IEnumerable<Bid>> GetByAuctionIdAsync(
+        int auctionId, int pageNumber, int pageSize, 
+        CancellationToken ct = default);
 }
--- AuctionR.Core.Infrastructure/Repositories/BidRepository.cs
+++ AuctionR.Core.Infrastructure/Repositories/BidRepository.cs
@@ -2,6 +2,7 @@
 using AuctionR.Core.Domain.Interfaces;
 using AuctionR.Core.Infrastructure.Persistance;
 using AuctionR.Shared.Repositories;
+using Microsoft.EntityFrameworkCore;
 
 namespace AuctionR.Core.Infrastructure.Repositories;
 
@@ -10,5 +11,16 @@ internal class BidRepository : Repository<AuctionRDbContext, Bid>, IBidRepositor
     public BidRepository(AuctionRDbContext context) 
         : base(context)
     {
+    }
+
+    public async Task<IEnumerable<Bid>> GetByAuctionIdAsync(
+        int auctionId, int pageNumber, int pageSize, CancellationToken ct = default)
+    {
+        return await _context.Bids
+            .Where(b => b.AuctionId == auctionId)
+            .OrderByDescending(b => b.Timestamp)
+            .Skip((pageNumber - 1) * pageSize)
+            .Take(pageSize)
+            .ToListAsync(ct);
     }
 }
