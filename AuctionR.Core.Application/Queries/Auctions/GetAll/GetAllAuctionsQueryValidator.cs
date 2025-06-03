fc64cc0f0be354503ef806d44863ff37566968c4
Giorgi Dolidze <gio.dolidze1234@gmail.com>
2025-06-03 14:03:15 +0400

    remove requirement

--- AuctionR.Core.Application/Queries/Auctions/GetAll/GetAllAuctionsQueryValidator.cs
+++ AuctionR.Core.Application/Queries/Auctions/GetAll/GetAllAuctionsQueryValidator.cs
@@ -7,11 +7,9 @@ public class GetAllAuctionsQueryValidator : AbstractValidator<GetAllAuctionsQuer
     public GetAllAuctionsQueryValidator()
     {
         RuleFor(x => x.PageNumber)
-            .NotEmpty().WithMessage("PageNumber is required.")
             .GreaterThan(0).WithMessage("PageNumber must be greater than 0.");
 
         RuleFor(x => x.PageSize)
-            .NotEmpty().WithMessage("PageSize is required.")
             .GreaterThan(0).WithMessage("PageSize must be greater than 0.");
     }
 }
