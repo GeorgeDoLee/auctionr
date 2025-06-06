e76c5b2cbd390b275387275ec1ef2c86268eccc0
Giorgi Dolidze <gio.dolidze1234@gmail.com>
2025-06-06 17:01:11 +0400

    publishing domain events

--- AuctionR.Core.Domain/Primitives/Entity.cs
+++ AuctionR.Core.Domain/Primitives/Entity.cs
@@ -6,6 +6,8 @@ public abstract class Entity
 
     private readonly List<DomainEvent> _domainEvents = new();
 
+    public ICollection<DomainEvent> DomainEvents => _domainEvents;
+
     protected void Raise(DomainEvent domainEvent)
     {
         _domainEvents.Add(domainEvent);
--- AuctionR.Core.Infrastructure/Persistance/AuctionRDbContext.cs
+++ AuctionR.Core.Infrastructure/Persistance/AuctionRDbContext.cs
@@ -1,13 +1,20 @@
 ﻿using AuctionR.Core.Domain.Entities;
+using AuctionR.Core.Domain.Primitives;
+using MediatR;
 using Microsoft.EntityFrameworkCore;
 
 namespace AuctionR.Core.Infrastructure.Persistance;
 
 internal class AuctionRDbContext : DbContext
 {
-    public AuctionRDbContext(DbContextOptions<AuctionRDbContext> options)
+    private readonly IPublisher _publisher;
+
+    public AuctionRDbContext(
+        DbContextOptions<AuctionRDbContext> options,
+        IPublisher publisher)
         : base(options)
     {
+        _publisher = publisher;
     }
 
     public DbSet<Auction> Auctions { get; set; }
@@ -19,5 +26,22 @@ internal class AuctionRDbContext : DbContext
         modelBuilder.ApplyConfigurationsFromAssembly(typeof(AuctionRDbContext).Assembly);
 
         base.OnModelCreating(modelBuilder);
+    }
+
+    public override async Task<int> SaveChangesAsync(CancellationToken ct = default)
+    {
+        var domainEvents = ChangeTracker.Entries<Entity>()
+            .Select(e => e.Entity)
+            .Where(e => e.DomainEvents.Any())
+            .SelectMany(e => e.DomainEvents);
+
+        var result = await base.SaveChangesAsync(ct);
+
+        foreach (var domainEvent in domainEvents)
+        {
+            await _publisher.Publish(domainEvent, ct);
+        }
+
+        return result;
     }
 }
